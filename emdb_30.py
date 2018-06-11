16fcfaa20152a5bd1a99b9995692394eae02183b
sanja <sanja@ebi.ac.uk>
2018-06-11 13:36:29 +0100

    created with --no-warnings

--- emdb_30.py
+++ emdb_30.py
@@ -2,20 +2,21 @@
 # -*- coding: utf-8 -*-
 
 #
-# Generated Mon Jun 11 11:39:21 2018 by generateDS.py version 2.29.5.
+# Generated Mon Jun 11 13:32:49 2018 by generateDS.py version 2.29.5.
 # Python 2.7.11 (v2.7.11:6d1b6a68f775, Dec  5 2015, 12:54:16)  [GCC 4.2.1 (Apple Inc. build 5666) (dot 3)]
 #
 # Command line options:
 #   ('--root-element', 'emd')
 #   ('-f', '')
 #   ('-o', '../../IdeaProjects/cif-emdb-translator/emdb.py')
+#   ('--no-warnings', '')
 #   ('--external-encoding', 'utf-8')
 #
 # Command line arguments:
 #   ../../IdeaProjects/emdb-schemas/v3/v3_0_0_0/emdb.xsd
 #
 # Command line:
-#   /Library/Frameworks/Python.framework/Versions/2.7/bin/generateDS.py --root-element="emd" -f -o "../../IdeaProjects/cif-emdb-translator/emdb.py" --external-encoding="utf-8" ../../IdeaProjects/emdb-schemas/v3/v3_0_0_0/emdb.xsd
+#   /Library/Frameworks/Python.framework/Versions/2.7/bin/generateDS.py --root-element="emd" -f -o "../../IdeaProjects/cif-emdb-translator/emdb.py" --no-warnings --external-encoding="utf-8" ../../IdeaProjects/emdb-schemas/v3/v3_0_0_0/emdb.xsd
 #
 # Current working directory (os.getcwd()):
 #   generateDS-2.23a0
